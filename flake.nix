0d81f9acf43a1728e15e8136dce0aea4e5a452b6
Thomas Bach <info@thomasbach.dev>
2021-07-08 18:11:07 +0200

    feat(init): addad nix flake infrastructure

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Flake using Input Output HK infrastructure";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          opticsExercises =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc8104";
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      flake = pkgs.opticsExercises.flake {};
+    in flake // {
+      # Built by `nix build .`
+      defaultPackage = flake.packages."lib:optics-exercises";
+
+      # This is used by `nix develop .` to open a shell for use with
+      # `cabal`, `hlint` and `haskell-language-server`
+      devShell = pkgs.opticsExercises.shellFor {
+        withHoogle = true;
+        tools = {
+          cabal = "latest";
+          hlint = "latest";
+          haskell-language-server = "latest";
+        };
+      };
+    });
+}
