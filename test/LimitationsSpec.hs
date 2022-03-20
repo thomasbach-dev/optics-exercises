9db31d6ed535debffb52ff11ba2c3c4ff99b0b48
Thomas Bach <info@thomasbach.dev>
2022-03-20 22:06:49 +0100

    test: limitations

---
+++ test/LimitationsSpec.hs
@@ -0,0 +1,19 @@
+module LimitationsSpec (spec) where
+
+import Control.Lens           (set, view)
+import Test.Hspec             (Spec, describe, it, shouldBe)
+import Test.Validity.Property (forAllValid)
+
+import Limitations
+
+spec :: Spec
+spec = do
+  describe "conditional" $ do
+    it "should get the right field" $ forAllValid $ \t@(x, y, z) ->
+      let expected :: Int
+          expected = if x then y else z
+      in view conditional t `shouldBe` expected
+    it "should set the second field when True" $ forAllValid $ \(x, y, z) ->
+      set conditional x (True, y, z) `shouldBe` (True, x, z :: Int)
+    it "should set the third field when False" $ forAllValid $ \(x, y, z) ->
+      set conditional x (False, y, z) `shouldBe` (False, y, x :: Int)
