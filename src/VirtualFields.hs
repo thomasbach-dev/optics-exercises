acdc520c9a452652c44e277e78be27324d631ac1
Thomas Bach <info@thomasbach.dev>
2022-05-02 04:38:48 +0200

    feat: drop username in User record
    This is the first exercise in the “Virtual Fields” chapter.

--- src/VirtualFields.hs
+++ src/VirtualFields.hs
@@ -6,14 +6,20 @@ module VirtualFields
   , User
   ) where
 
-import Control.Lens (makeLenses)
+import Control.Lens (Lens', makeLenses)
 import Data.Text    (Text)
 import GHC.Generics (Generic)
 
 data User = User
   { _firstName :: Text
   , _lastName  :: Text
-  , _username  :: Text
   , _email     :: Text
   } deriving (Eq, Generic, Show)
 makeLenses ''User
+
+-- We’ve decided we’re no longer going to have separate usernames and emails; now the email will be
+-- used in place of a username. Your task is to delete the _username field and write a replacement
+-- username lens which reads and writes from/to the _email field instead. The change should be
+-- unnoticed by those importing the module.
+username :: Lens' User Text
+username = email
